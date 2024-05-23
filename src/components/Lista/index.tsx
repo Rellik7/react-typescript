36bbc8b50a7c4c7c694f68d5784ab78d3a346bc6
Rafael Vasconcelos <69718796+Rellik7@users.noreply.github.com>
2024-05-23 14:08:31 -0300

    Commit aula 05

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
+import { ITarefa } from '../../types/ITarefa';
 
-export default function Lista() {
-  const tarefas = [{
-    tarefa: 'React',
-    tempo: '02:00:00',
-  },
-  {
-    tarefa: 'Javascript',
-    tempo: '01:00:00',
-  },
-  {
-    tarefa: 'Typescript',
-    tempo: '03:00:00',
-  }]
 
+export default function Lista({ tarefas }: { tarefas: ITarefa[] } ) {
   return (
     <aside className={style.listaTarefas}>
       <h2> Estudos do dia</h2>
